1ee08929ee363788d604f1209c5392cc1d605dbb
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-09-01 12:23:39 -0300

    Provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 
 const App = () => (
-  <main>
-    <h1>ED manager</h1>
-    <Jugadores />
-    <EquipoSeleccionado />
-  </main>
+  <Provider store={store}>
+    <main>
+      <h1>ED manager</h1>
+      <Jugadores />
+      <EquipoSeleccionado />
+    </main>
+  </Provider>
 )
 
 export default App;
