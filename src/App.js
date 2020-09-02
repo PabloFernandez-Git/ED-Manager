8a909699f33bd1ffd1e20d50ae727000b94b27ce
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-09-02 12:24:17 -0300

    Funcionalidad terminada

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import Jugadores from './components/Jugadores';
 import EquipoSeleccionado from './components/EquipoSeleccionado';
+import './styles/styles.scss'
 
 
 const App = () => (
